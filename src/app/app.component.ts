e6a9d0faffd9ebe1921676207774befb267c27fc
Sashko <sashkoace@gmail.com>
2020-10-20 16:43:13 +0300

    step 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,27 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
   urls = [];
 
-  selectImgs(event) {
-    if (event.target.files) {
-      console.log(event.target.files);
-
+  selectImgs(e) {
+    if (e.target.files) {
       for (let i = 0; i < File.length; i++) {
         const reader = new FileReader();
-        reader.readAsDataURL(event.target.files[i]);
-        reader.onload = (events: any) => {
-          this.urls.push(events.target.rasult);
+        reader.readAsDataURL(e.target.files[i]);
+        reader.onload = (e: any) => {
+          this.urls.push(reader.result);
         }
       }
     }
